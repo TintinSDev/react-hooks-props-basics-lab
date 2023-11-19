3ef9097f9fcafc8785bda6f819d3071c7a4bfe93
Martin <mmaina290@gmail.com>
2023-11-19 15:38:29 +0300

    All tests passed

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,7 +6,7 @@ function Links() {
         <div>
             <h3>Links</h3>
             <a href="https://github.com/liza">https://github.com/liza</a>
-            <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza</a>
+            <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a>
         </div>
     )
 }
